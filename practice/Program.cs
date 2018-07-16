75367e9fd9e65e118554ecf31ff7fc93a2c23560
Eddie Crochet <eddiecrochet1994@gmail.com>
2018-07-16 18:54:53 -0500

    first commit for week 2

---
+++ practice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("aye, what up Practice week 2!!!");
+        }
+    }
+}
