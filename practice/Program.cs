67b3a86e9f87b23a13bc27d475cc8127cc9090a9
Eddie Crochet <eddiecrochet1994@gmail.com>
2018-07-16 20:19:22 -0500

    this was just the in class practice

--- practice/Program.cs
+++ practice/Program.cs
@@ -6,7 +6,46 @@ namespace practice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("aye, what up Practice week 2!!!");
+//GOT TO CALL THE FUNCTIONS HERE (or wherever you want them)
+substringExample();
+IndexOfExample();
+JoinExample();
+        }
+
+        static void substringExample()
+        {
+            string haystack = "some long string";
+            int index = 3;
+            int length = 10;
+            string subString1 = haystack.Substring(index);
+            string subString2 = haystack.Substring(index, length);
+            Console.WriteLine("substring1 = {0}", subString1);
+            Console.WriteLine("substring2 = {0}", subString2);
+        }
+
+        static void IndexOfExample()
+        {
+            string haystack = "The quick brown fox jumps over the lazy dog.";
+            string needle = "a";
+            int index = haystack.IndexOf(needle);
+            Console.WriteLine("found the needle starting at position {0}", index );
+        }
+           
+        static void JoinExample()
+        {
+            string fname = "John";
+            string lname = "Doe";
+            string mname = "Billy";
+
+            string[] nameParts = new string[3];
+            nameParts[0] = fname;
+            nameParts[1] = mname;
+            nameParts[2] = lname;
+
+            string separator = " ";
+
+            String fullName = String.Join(separator, nameParts);
+            Console.WriteLine(fullName);
         }
     }
 }
