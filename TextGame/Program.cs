8b9c013ee6aa85a275aa4a116e277a7fd3d86c89
Eddie Crochet <eddiecrochet1994@gmail.com>
2018-07-18 04:34:24 -0500

    To my knowledge it seems it should work. However I'm getting a confusing error I'm hoping you can help me out with

--- TextGame/Program.cs
+++ TextGame/Program.cs
@@ -6,40 +6,24 @@ namespace TextGame
     {
         static void Main(string[] args)
         {
-            /*  
-import time
-import random
-
-# game function
-def game():
-
-print ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-print ("Welcome to the cavern of secrets!")
-print ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-*/
+            
 Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 Console.WriteLine("Welcome to the cavern of secrets!");     //intro to the game
 Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 
 string ch1 = System.String.Empty;
 string ch2 = System.String.Empty;           //assigning variables
+string ch3 = System.String.Empty;
+string LpGm = System.String.Empty;
 int stick = 0;
+int complete = 0;
+
 
 System.Threading.Thread.Sleep(3000); //time.sleep(3) ... the game waits a bit
-/*
-print ("You enter a dark cavern out of curiosity. It is dark and you can only make out a small stick on the floor.")
-ch1 = str(input("Do you take it? [y/n]: "))
-*/
+
 Console.WriteLine("You enter a dark cavern out of curiosity. It is dark and you can only make out a small stick on the floor.");
 Console.WriteLine("Do you take it?? [y/n]");
 ch1 = Console.ReadLine();
-/*
-# STICK TAKEN
-if ch1 in ['y', 'Y', 'Yes', 'YES', 'yes']:
-    print("You have taken the stick!")
-    time.sleep(2)
-    stick = 1
-    */
 
 //if the stick is taken
 if (ch1 == "y"){
@@ -50,146 +34,114 @@ if (ch1 == "y"){
     Console.WriteLine("You did not take the stick");
     stick = 0;
 }
-/*
-# STICK NOT TAKEN
-else:
-    print("You did not take the stick")
-    stick = 0
 
-print ("As you proceed further into the cave, you see a small glowing object")
-ch2 = str(input("Do you approach the object? [y/n]"))
-
-*/
+//continuing on with our adventure
 Console.WriteLine("As you proceed further into the cave, you see a small glowing object");
 Console.WriteLine("Do you approach the object? [y/n]");
 ch2 = Console.ReadLine();
-/*
 
-# APPROACH SPIDER
-if ch2 in ['y', 'Y', 'Yes', 'YES', 'yes']:
-    print ("You approach the object...")
-    time.sleep(2)
-    print ("As you draw closer, you begin to make out the object as an eye!")
-    time.sleep(1)
-    print ("The eye belongs to a giant spider!")
-    ch3 = str(input("Do you try to fight it? [Y/N]"))
-
-    */
     if (ch2 == "y"){        //APPROACH SPIDER
         Console.WriteLine("You approach the object...");
         System.Threading.Thread.Sleep(2000);
         Console.WriteLine("As you draw closer, you begin to make out the object as an eye!");
         System.Threading.Thread.Sleep(1000);
         Console.WriteLine("The eye belongs to a giant spider!");
+        Console.WriteLine("Do you try to fight it? [Y/N]");
+        ch3 = Console.ReadLine();
+        if (ch3 == "y"){     //IF YOU DECIDE TO FIGHT THE SPIDER
+            if (stick == 1){        //FIGHT SPIDER AND HAVE A STICK
+                Console.WriteLine("You only have a stick to fight with!");
+                Console.WriteLine("You quickly jab the spider in it's eye and gain an advantage!");
+                System.Threading.Thread.Sleep(2000);
+                Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                Console.WriteLine("                  Fighting...                   ");
+                Console.WriteLine("   YOU MUST HIT ABOVE A 5 TO KILL THE SPIDER    ");
+                Console.WriteLine("IF THE SPIDER HITS HIGHER THAN YOU, YOU WILL DIE");
+                Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                System.Threading.Thread.Sleep(2000);
+                Random rng = new Random();
+                int playerDmg = rng.Next(3, 10);
+                int spiderDmg = rng.Next(1, 5);
+                Console.WriteLine("Your hit does " + playerDmg + " damage!");
+                Console.WriteLine("The spiders hit does " + spiderDmg + " damage!");
+                System.Threading.Thread.Sleep(2000);
+                    if (spiderDmg > playerDmg){     //IF SPIDER DEALS MORE DAMAGE THAN YOU
+                        Console.WriteLine("The spider has dealt more damage than you!");
+                        System.Threading.Thread.Sleep(1000);
+                        complete = 0;
+                        return complete;
+                    }else if (playerDmg < 5){       //NOT ENOUGH DMG TO KILL SPIDER BUT SURVIVED BATTLE
+                        Console.WriteLine("You didn't kill the spider but you manage to escape.");
+                        complete = 1;
+                        return complete;
+                    }else{          //BEAT THE SPIDER IN BATTLE
+                        Console.WriteLine("You killed the spider!"); 
+                        complete = 1;
+                        return complete;      
+                    }
+            }else if (stick == 0){          //FIGHT SPIDER AND HAVE NO STICK
+                Console.WriteLine("You don't have anything to fight with!");
+                System.Threading.Thread.Sleep(2000);
+                Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                Console.WriteLine("                  Fighting...                   ");
+                Console.WriteLine("   YOU MUST HIT ABOVE A 5 TO KILL THE SPIDER    ");
+                Console.WriteLine("IF THE SPIDER HITS HIGHER THAN YOU, YOU WILL DIE");
+                Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                System.Threading.Thread.Sleep(2000);
+                Random ran = new Random();
+                int playerDam = ran.Next(1, 8);
+                int spiderDam = ran.Next(1, 5);
+                Console.WriteLine("Your hit does " + playerDam + " damage!");
+                Console.WriteLine("The spiders hit does " + spiderDam + " damage!");
+                System.Threading.Thread.Sleep(2000);
+                if (spiderDam > playerDam){     //IF SPIDER DEALS MORE DAMAGE THAN YOU
+                    Console.WriteLine("The spider has dealt more damage than you!");
+                    complete = 0;
+                    return complete;
+                }else if (playerDam < 5){       //NOT ENOUGH TO KILL THE SPIDER BUT SURVIVED BATTLE
+                    Console.WriteLine("You didn't kill the spider but you managed to escape");
+                    complete = 1;
+                    return complete;
+                }else{           //KILLED TH SPIDER IN BATTLE
+                    Console.WriteLine("You killed the spider!");
+                    complete = 1;
+                    return complete;
+                }
+            }
+        }else{      //DECIDE NOT TO FIGHT THE SPIDER
+            Console.WriteLine("You choose not to fight the spider...");
+            System.Threading.Thread.Sleep(1000);
+            Console.WriteLine("As you turn away, it ambushes you and impales you with it's fangs!!!");
+            complete = 0;
+            return complete;
+        }
     }else{  //DO NOT APPROACH SPIDER
         Console.WriteLine("You turn away from the glowing object, and attempt to leave the cave...");
+        System.Threading.Thread.Sleep(2000);
+        Console.WriteLine("But something won't let you....");
+        System.Threading.Thread.Sleep(2);
+        complete = 0;
+        return complete;
     }
 
+//GAME LOOP
+string LpGm = System.Class.Empty;
+bool alive = True;
+while (alive == True){
+    if (complete == 1){
+        Console.WriteLine("You managed to escape the cavern alive! Would you like to play again? [y/n]:");
+        alive = Convert.ToBoolean(Console.ReadLine());
+        if (LpGm == "y"){ //IF YOU CHOSE TO PLAY AGAIN IT LOOPS THE MAIN GAME BACK TO THE START
+            Main();
+        }else{
+            break;
+        }
+    }
+    
+}else{
+    break;
+}
 
-    /*
-
-    # FIGHT SPIDER
-    if ch3 in ['y', 'Y', 'Yes', 'YES', 'yes']:
-
-        # WITH STICK
-        if stick == 1:
-            print ("You only have a stick to fight with!")
-            print ("You quickly jab the spider in it's eye and gain an advantage")
-            time.sleep(2)
-            print ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-            print ("                  Fighting...                   ")
-            print ("   YOU MUST HIT ABOVE A 5 TO KILL THE SPIDER    ")
-            print ("IF THE SPIDER HITS HIGHER THAN YOU, YOU WILL DIE")
-            print ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-            time.sleep(2)
-            fdmg1 = int(random.randint(3, 10))
-            edmg1 = int(random.randint(1, 5))
-            print ("you hit a", fdmg1)
-            print ("the spider hits a", edmg1)
-            time.sleep(2)
-
-            if edmg1 > fdmg1:
-                print ("The spider has dealt more damage than you!")
-                complete = 0
-                return complete
-
-            elif fdmg1 < 5:
-                print ("You didn't do enough damage to kill the spider, but you manage to escape")
-                complete = 1
-                return complete
-
-            else:
-                print ("You killed the spider!")
-                complete = 1
-                return complete
-
-        # WITHOUT STICK
-        else:
-            print ("You don't have anything to fight with!")
-            time.sleep(2)
-            print ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-            print ("                  Fighting...                   ")
-            print ("   YOU MUST HIT ABOVE A 5 TO KILL THE SPIDER    ")
-            print ("IF THE SPIDER HITS HIGHER THAN YOU, YOU WILL DIE")
-            print ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-            time.sleep(2)
-            fdmg1 = int(random.randint(1, 8))
-            edmg1 = int(random.randint(1, 5))
-            print ("you hit a", fdmg1)
-            print ("the spider hits a", edmg1)
-            time.sleep(2)
-
-            if edmg1 > fdmg1:
-                print ("The spider has dealt more damage than you!")
-                complete = 0
-                return complete
-
-            elif fdmg1 < 5:
-                print ("You didn't do enough damage to kill the spider, but you manage to escape")
-                complete = 1
-                return complete
-
-            else:
-                print ("You killed the spider!")
-                complete = 1
-                return complete
-
-    #DON'T FIGHT SPIDER
-    print ("You choose not to fight the spider.")
-    time.sleep(1)
-    print ("As you turn away, it ambushes you and impales you with it's fangs!!!")
-    complete = 0
-    return complete
-
-# DON'T APPROACH SPIDER
-else:
-        print ("You turn away from the glowing object, and attempt to leave the cave...")
-        time.sleep(1)
-        print ("But something won't let you....")
-        time.sleep(2)
-        complete = 0
-        return complete
-
-# game loop
-alive = True
-while alive:
-
-complete = game()
-if complete == 1:
-    alive = input('You managed to escape the cavern alive! Would you like to play again? [y/n]: ')
-    if alive in ['y', 'Y', 'YES', 'yes', 'Yes',]:
-        alive
-
-    else:
-        break
-
-else:
-    alive = input('You have died! Would you like to play again? [y/n]: ')
-    if alive in ['y', 'Y', 'YES', 'yes', 'Yes',]:
-        alive
-
-    else:
-        break  */
         }
     }
 }
