cc3cab3fa7a318ddc726ab8ba356938fa865d6fc
Eddie Crochet <eddiecrochet1994@gmail.com>
2018-08-12 15:46:17 -0500

    just created the branch for the gradebook

--- gradebook/Program.cs
+++ gradebook/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace gradebook
 {
@@ -6,7 +7,8 @@ namespace gradebook
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please enter your students names one at a time and enter ok when yoiu are done.");
+
         }
     }
 }
