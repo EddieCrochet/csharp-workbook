d4ad66288bcdb6e057fb64c0736ab31f5fb3fa49
Eddie Crochet <eddiecrochet1994@gmail.com>
2018-08-13 18:18:16 -0500

    jumping around

--- gradebook/Program.cs
+++ gradebook/Program.cs
@@ -7,8 +7,8 @@ namespace gradebook
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please enter your students names one at a time and enter ok when yoiu are done.");
-
+            Console.WriteLine("Please enter your students names one at a time and enter ok when you are done.");
+            
         }
     }
 }
