4a8f851582fde5b9488d6dc8676b6c1a59746ada
Eddie Crochet <eddiecrochet1994@gmail.com>
2018-08-14 18:29:22 -0500

    making progress

--- gradebook/Program.cs
+++ gradebook/Program.cs
@@ -8,6 +8,20 @@ namespace gradebook
         static void Main(string[] args)
         {
             Console.WriteLine("Please enter your students names one at a time and enter ok when you are done.");
+
+            //creates a new dictionary to store our gradebook
+            Dictionary <String, int> gradeBook = new Dictionary<String, int>();
+            string userInput = Console.ReadLine();
+
+            //creates stack to store the names of students
+            Stack<String> students = new Stack<String>();
+            
+            do
+            {
+                students.Push(userInput);
+            }
+            while(userInput != "ok");
+            
             
         }
     }
