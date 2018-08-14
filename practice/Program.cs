311509cc62e4dc4ec7616cb640b14b5edc4958b9
Eddie Crochet <eddiecrochet1994@gmail.com>
2018-08-13 20:46:52 -0500

    in class practice

---
+++ practice/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace practice
+{
+    class Vehicle
+    {
+        public int engineSize;
+        public int numTires;
+        public int passengerCapacity;
+
+        public Vehicle(int engineSize, int numTires, int passengerCapacity)
+        {
+            this.engineSize = engineSize;
+            this.numTires = numTires;
+            this.passengerCapacity = passengerCapacity;
+        }
+    }
+
+    class Truck : Vehicle
+    {
+        int bedSize;
+        int towingCapacity;
+
+        public Truck(int bedSize, int towingCapacity, int engineSize, int numTires, int passengerCapacity)
+        :base(engineSize, numTires, passengerCapacity)
+        {
+            this.bedSize = bedSize;
+            this.towingCapacity = towingCapacity;
+        }
+    }
+
+    class Sedan : Vehicle
+    {
+        bool luxury;
+
+        public Sedan(bool luxury, int engineSize, int passengerCapacity)
+        : base(engineSize, 4, passengerCapacity)
+        {
+            this.luxury = luxury;
+        }
+    }
+
+    class Motorcycle : Vehicle
+    {
+        bool cruiser;
+
+        public Motorcycle(bool cruiser, int engineSize, int passengerCapacity)
+        : base(engineSize, 2, passengerCapacity)
+        {
+            this.cruiser = cruiser;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
