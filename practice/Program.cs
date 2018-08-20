d3f727728f8186752efa6ee9b715a9ef340a4550
Eddie Crochet <eddiecrochet1994@gmail.com>
2018-08-20 18:34:42 -0500

    started the qiz

--- practice/Program.cs
+++ practice/Program.cs
@@ -6,7 +6,27 @@ namespace practice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("success");
+        }
+    }
+
+    class Car
+    {
+        public double hourlyRentalRate {get; set;}
+
+        public Car(double HourlyRentalRate)
+        {
+            this.hourlyRentalRate = HourlyRentalRate;
+        }
+    }
+
+    class HotelRoom
+    {
+        public int nightlyRentalRate{get; set;}
+
+        public HotelRoom(int NightlyRentalRate)
+        {
+            this.nightlyRentalRate = NightlyRentalRate;
         }
     }
 }
