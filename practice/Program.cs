713564f8dacf88ccf322a9c7788b33db94ce521e
Eddie Crochet <eddiecrochet1994@gmail.com>
2018-08-21 17:42:53 -0500

    from class

--- practice/Program.cs
+++ practice/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace practice
 {
@@ -6,32 +7,101 @@ namespace practice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("success");
+            Car mercedes = new Car("fghjk", 140.67);
+            Car lambo = new Car("gfds", 220.23);
+            Car fiesta = new Car("yuio", 23.20);
+
+            HotelRoom suite = new HotelRoom("1001", 650);
+            HotelRoom standard1 = new HotelRoom("65", 330);
+            HotelRoom standard2 = new HotelRoom("65", 333);
+
+            List<Rentable> rentableThings = new List<Rentable>();
+            rentableThings.Add(mercedes);
+            rentableThings.Add(lambo);
+            rentableThings.Add(fiesta);
+            rentableThings.Add(suite);
+            rentableThings.Add(standard1);
+            rentableThings.Add(standard2);
+
+
+//james' way
+            // foreach(Rentable thing in rentableThings)
+            // {
+            //     Console.WriteLine("if you rent this {0} for 1 day, you owe {1}",
+            //     thing.GetType().Name,
+            //     thing.calculateRent(1));
+            // }
+            foreach(Rentable thing in rentableThings)
+            {
+                String thingType = "";
+                if(thing is Car)
+                {
+                    Car temp = (Car) thing;
+                    thingType = "car";
+                } 
+                else if (thing is HotelRoom)
+                {
+                    thingType = "hotel room";
+                }
+            }
+
+
+            Console.WriteLine("For car {0} for 1 day the amount due is {1}.", 
+            mercedes.licenseNo, mercedes.calculateRent(1));
+
+            Console.WriteLine("For car {0} for 1 day the amount due is {1}.", 
+            lambo.licenseNo, lambo.calculateRent(1));
+
+            Console.WriteLine("For car {0} for 1 day the amount due is {1}.", 
+            fiesta.licenseNo, fiesta.calculateRent(1));
+
+            Console.WriteLine("For room {0} for 1 day the amount due is {1}.", 
+            suite.roomNo, suite.calculateRent(1));
+            Console.WriteLine("For room {0} for 1 day the amount due is {1}.", 
+            standard1.roomNo, standard1.calculateRent(1));
         }
     }
 
-    class Car
+    public interface Rentable
     {
-        public double hourlyRentalRate {get; set;}
+        //double getDailyRate();
+        double calculateRent(double daysToRent);
+    }
 
-        public Car(double HourlyRentalRate)
+    public class Car : Rentable
+    {
+        public string licenseNo{get; private set;}
+        public double hourlyRentalRate {get; private set;}
+        //keeps track of particular instance of car for this attribute
+
+        public Car(string LicenseNo, double rate)
         {
-            this.hourlyRentalRate = HourlyRentalRate;
+            this.licenseNo = LicenseNo;
+            this.hourlyRentalRate = rate;
         }
 
-        public static rent(double days)
+        public double calculateRent(double daysToRent)
         {
-            
+            double dailyRate = 24*hourlyRentalRate;
+            //instance of car already had hourlyRentalRate var so we dont have to pass
+            return dailyRate*daysToRent;
         }
     }
 
-    class HotelRoom
+    class HotelRoom : Rentable
     {
-        public int nightlyRentalRate{get; set;}
+        public string roomNo {get; private set;}
+        public double dailyRate{get; private set;}
 
-        public HotelRoom(int NightlyRentalRate)
+        public HotelRoom(string RoomNo, double rate)
         {
-            this.nightlyRentalRate = NightlyRentalRate;
+            this.roomNo = RoomNo;
+            this.dailyRate = rate;
         }
+        public double calculateRent(double daysToRent)
+        {
+            return dailyRate * daysToRent;
+        }
+
     }
 }
