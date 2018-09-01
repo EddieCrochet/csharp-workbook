d1c60299411eec7c614b43d76b366b909c36d9be
Eddie Crochet <eddiecrochet1994@gmail.com>
2018-08-31 20:45:16 -0500

    Class work for file handling

---
+++ practice/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiated the reader
+            StreamReader reader = new StreamReader(@"C:\\ThisPC\Desktop\words_alpha.txt");
+            //create a reader and a copied version of data in writer to start with
+            //instantiate a writer
+            StreamWriter writer = new StreamWriter(@"C:\\ThisPC\Desktop\words.copy.txt");
+
+            String line;
+
+            while((line = reader.ReadLine()) != null)
+            {
+                writer.WriteLine(line);
+            }
+
+            reader.Close();
+            writer.Flush();
+            writer.Close();
+        }
+    }
+}
