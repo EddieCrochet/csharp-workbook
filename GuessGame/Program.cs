21b1fa5b1681e09a0c2eb11208b27008b8b55e61
Eddie Crochet <eddiecrochet1994@gmail.com>
2018-09-02 18:30:29 -0500

    making progress on my guess game

---
+++ GuessGame/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+
+namespace GuessGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines(@"C:\Users\Eddie\Desktop\words_alpha.txt");
+            //reads all our lines from the files into an array
+
+            Random rng = new Random();
+            int r = rng.Next(lines.Length);
+            //creates a random number and selects a word for that number from our array of words
+            string compWord = lines[r];
+
+            Console.WriteLine("I have a word. Guess in the console and I will give you hints along the way.");
+            Console.WriteLine("My word I picked is {0}", compWord);
+            string userGuess = Console.ReadLine();
+
+            int compNum = Array.IndexOf(lines, compWord);
+            int userNum = Array.IndexOf(lines, userGuess);
+
+            bool CheckWin()
+            {
+                if(userNum == compNum)
+                {
+                return true;
+                }
+                return false;
+            }
+
+            bool won = true;
+            if(!CheckWin())
+            {         
+                if(compNum < userNum)
+                {
+                    Console.WriteLine("That guess comes after my word!");
+                }
+                else if(userNum < compNum)
+                {
+                    Console.WriteLine("That guess comes before my word!");
+                }
+                else
+                {
+                    Console.WriteLine("Good Lawd! You got my word!");
+                }
+            }
+        }
+    }
+}
--- practice/Program.cs
+++ practice/Program.cs
@@ -6,23 +6,59 @@ namespace practice
     class Program
     {
         static void Main(string[] args)
-        {
-            //instantiated the reader
-            StreamReader reader = new StreamReader(@"C:\Users\Eddie\Desktop\words_alpha.txt");
-            //create a reader and a copied version of data in writer to start with
-            //instantiate a writer
-            StreamWriter writer = new StreamWriter(@"C:\Users\Eddie\Desktop\words.copy.txt");
-
-            String line;
-
-            while((line = reader.ReadLine()) != null)
+        {           
+            string myFile = @"C:\Users\Eddie\Desktop\myFile.txt";
+            //code to create a text file
+            using(StreamWriter sw = File.CreateText(myFile))
             {
-                writer.WriteLine(line);
+                sw.WriteLine("New file created: {0}", DateTime.Now.ToString());
+                sw.WriteLine("Author: Eddie Crochet");
+                sw.WriteLine("and whatever the hell else i want to say");
             }
 
-            reader.Close();
-            writer.Flush();
-            writer.Close();
+            using(StreamReader sr = File.OpenText(myFile))
+            //prints out the created file into the console in a controlled manner
+            //but only prints before the edit
+            {
+                string line;
+
+                while((line = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            Console.WriteLine();
+
+            string changeText = File.ReadAllText(@"C:\Users\Eddie\Desktop\myFile.txt");
+            //makes a string named changeText to store my data from myFile
+            //here we edit two lines of text within our created file
+            changeText = changeText.Replace("Eddie Crochet", "Edward Laurence Crochet");
+            changeText = changeText.Replace("hell", "heck");
+            File.WriteAllText(@"C:\Users\Eddie\Desktop\myFile.txt", changeText);
+
+            using(StreamReader sr2 = File.OpenText(myFile))
+            //reprint to the console the created file - now complete with edits
+            {
+                string line;
+
+                while((line = sr2.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+
+            if(File.Exists(myFile))
+            //deletes the myFile if it exists
+            {
+                try
+                {
+                    File.Delete(myFile);
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+            }
         }
     }
 }
