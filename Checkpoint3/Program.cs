4793164aef173c00534df6d845a07de5ddf8f389
Eddie Crochet <eddiecrochet1994@gmail.com>
2018-09-13 00:00:28 -0500

    Began making a shell and white board for the project- I also introduced microsoft.entityFrameworkCore.Sqlie into the project

---
+++ Checkpoint3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.Data.Sqlite;
+
+namespace Checkpoint3
+{
+    public enum DoneStatus {undone, inProgress, done}
+    class Item
+    {
+        int id {get; set;}
+        string name {get; set;}
+        string description {get; set;}
+        DoneStatus Donestatus {get; set;}
+
+        public Item (int Id, string Name, string Description, doneStatus DoneStatus)
+        {
+            id = Id;
+            name = Name;
+            description = Description;
+            DoneStatus = DoneStatus;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqliteConnectionStringBuilder stringBuilder = new SqliteConnectionStringBuilder();
+
+            List<Item> toDoList = new List<Item>();
+            Console.WriteLine("Please enter a line you'd like to enter into the list.");
+        }
+    }
+}
